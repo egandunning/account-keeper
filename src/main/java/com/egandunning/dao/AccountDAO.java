e487d43289003e3c4b3c2e2149ae744a2517bb83
Egan <dunninea@plu.edu>
2018-04-02 08:37:47 -0500

    created dao interface for accounts

---
+++ src/main/java/com/egandunning/dao/AccountDAO.java
@@ -0,0 +1,16 @@
+package com.egandunning.dao;
+
+import java.util.List;
+
+import com.egandunning.beans.Account;
+
+public interface AccountDAO {
+
+    public List<Account> getAccounts(String owner) throws Exception;
+    
+    public Account getAccountById(String owner, long id) throws Exception;
+    
+    public void createAccount(Account account) throws Exception;
+    
+    public void updateAccount(long id, Account account) throws Exception;
+}
