91025dfef8bc06e735100cb47bf2bb448b3aca3c
Egan <dunninea@plu.edu>
2018-04-19 19:11:35 -0500

    added dummy dao implementation

---
+++ src/main/java/com/egandunning/dao/BackupDAOImpl.java
@@ -0,0 +1,38 @@
+package com.egandunning.dao;
+
+import java.util.List;
+
+import com.egandunning.beans.Backup;
+
+public class BackupDAOImpl implements BackupDAO {
+
+    @Override
+    public List<Backup> getBackups(String owner) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Backup getBackup(String owner, long id) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Backup getNewestBackup(String owner) throws Exception {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void createBackup(Backup backup) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void deleteBackup(long id) throws Exception {
+        // TODO Auto-generated method stub
+        
+    }
+}
